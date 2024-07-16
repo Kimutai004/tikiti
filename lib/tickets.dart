041721d3bd01aba8e6bf4a2c139f14e285217d93
Peter Kimutai <peterkimutai004@gmail.com>
2024-07-16 13:31:31 +0300

    commit

--- lib/tickets.dart
+++ lib/tickets.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tikiti/index.dart' as tikiti;
 import 'package:tikiti/profile.dart';
+import 'dart:io';
 import 'package:tikiti/search.dart';
 
 class Tickets extends StatefulWidget {
@@ -103,107 +104,140 @@ class _TicketsState extends State<Tickets> {
                   ),
                 ),
                 StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance
-          .collection('tickets')
-          
-    .where('user_id', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
-          .snapshots(),
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return Center(child: Text('No tickets found'));
-            }
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (BuildContext context, int index) {
-              DocumentSnapshot event = snapshot.data!.docs[index];
+          stream: FirebaseFirestore.instance
+            .collection('tickets')
+            .where('user_id', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+            .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('No tickets found'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+            DocumentSnapshot event = snapshot.data!.docs[index];
 
-              // Ensure all required fields are present
-              String eventPoster = event.data().toString().contains('event_poster') ? event['event_poster'] : '';
-              String eventTitle = event.data().toString().contains('event_title') ? event['event_title'] : 'Untitled Event';
-              String eventDate = event.data().toString().contains('date') ? event['date'] : 'Date not available';
-              String price = event.data().toString().contains('price') ? event['price'] : '0.00';
-              
-              // Check if ticketType is a map or a string
-                String ticketType;
-                if (event['ticket_type'] is Map) {
-                ticketType = (event['ticket_type'] as Map<String, dynamic>).keys.join(', ');
-                } else {
-                ticketType = event['ticket_type'] ?? 'Unknown';
-                }
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
-                child: Container(
-                width: 107,
-                height: 75,
-                decoration: BoxDecoration(
-                  image: eventPoster.isNotEmpty
-                  ? DecorationImage(
-                    image: NetworkImage(eventPoster),
-                    fit: BoxFit.fill,
-                  )
-                  : null,
-                  color: eventPoster.isEmpty ? Colors.grey : null,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Text(
-                    eventTitle,
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Kavoon',
-                    fontWeight: FontWeight.w400,
-                    height: 1.0,
-                    ),
+            // Extracting fields with default values
+            String eventPoster = event['event_poster'] ?? '';
+            String eventTitle = event['event_title'] ?? 'Untitled Event';
+            String price = event['price'] ?? '0.00';
+            
+            // Check if ticketType is a map or a string
+            String ticketType;
+            if (event['ticket_type'] is Map) {
+              ticketType = (event['ticket_type'] as Map<String, dynamic>).keys.join(', ');
+            } else {
+              ticketType = event['ticket_type'] ?? 'Unknown';
+            }
+
+            return GestureDetector(
+              onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(eventTitle),
+                  content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                    // Check if eventPoster is not empty and display the image from a file
+          eventPoster.isNotEmpty
+            ? Image.file(File(eventPoster))
+            : SizedBox.shrink(),
+          Text('Price: $price'),
+          Text('Type: $ticketType'),
+                    // display qr code
+                    ],
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    eventDate,
-                    style: TextStyle(
-                    color: Color(0xFFFF3D00),
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                    height: 1.0,
-                    ),
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Price: $price',
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                    height: 1.0,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Type: $ticketType',
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                    height: 1.0,
-                    ),
+                  actions: <Widget>[
+                  TextButton(
+                    child: Text('Close'),
+                    onPressed: () {
+                    Navigator.of(context).pop();
+                    },
                   ),
                   ],
-                ),
-                ),
+                );
+                },
               );
               },
+              child: Padding(
+              padding: EdgeInsets.only(top: 10, left: 24, right: 24),
+              child: 
+        Row(
+          children: [
+          // Image container
+          Container(
+            width: 107,
+            height: 75,
+            decoration: BoxDecoration(
+            image: eventPoster.isNotEmpty
+              ? DecorationImage(
+                image: FileImage(File(eventPoster)),
+                fit: BoxFit.cover, // Ensure the image covers the box
+                )
+              : null,
+            color: eventPoster.isEmpty ? Colors.grey : null,
+            ),
+          ),
+          // Spacer or SizedBox can be used to create some space between the image and text
+          SizedBox(width: 8),
+          // Texts container
+          Expanded( // Use Expanded to ensure the column takes the rest of the space
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start (left)
+            mainAxisAlignment: MainAxisAlignment.center, // Center the column
+            children: [
+              Text(
+              eventTitle,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Kavoon',
+                fontWeight: FontWeight.w400,
+                height: 1.0,
+              ),
+              ),
+              SizedBox(height: 4),
+              Text(
+              'Price: $price',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
+                height: 1.0,
+              ),
+              ),
+              SizedBox(height: 4),
+              Text(
+              'Type: $ticketType',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
+                height: 1.0,
+              ),
+              ),
+            ],
+            ),
+          ),
+          ],
+        )
+              ),
             );
             },
-          ),
+          );
+          },
+        )
 
               ],
             ),
